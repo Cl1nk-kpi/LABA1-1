f3ce69ec48ae9d968d80dbf4b15e99434003feb1
Timur <ciganoktimur105@gmail.com>
2026-03-03 03:37:11 +0200

    додав 2 і 3 завдання

---
+++ ЗАвдання 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+class Program
+{
+    static void Main()
+    {
+        List<Dictionary<string, string>> inputData = new List<Dictionary<string, string>>
+        {
+            new Dictionary<string, string> { { "V", "S001" } },
+            new Dictionary<string, string> { { "V", "S002" } },
+            new Dictionary<string, string> { { "VI", "S001" } },
+            new Dictionary<string, string> { { "VI", "S005" } },
+            new Dictionary<string, string> { { "VII", "S005" } },
+            new Dictionary<string, string> { { "V", "S009" } },
+            new Dictionary<string, string> { { "VIII", "S007" } }
+        };
+
+        HashSet<string> uniqueValues = new HashSet<string>();
+
+        foreach (var dict in inputData)
+        {
+            foreach (var value in dict.Values)
+            {
+                uniqueValues.Add(value);
+            }
+        }
+
+        Console.WriteLine($"UniqueValues: {{'{string.Join("', '", uniqueValues)}'}}");
+
+        Dictionary<string, HashSet<string>> resultDictionary = new Dictionary<string, HashSet<string>>
+        {
+            { "UniqueValues", uniqueValues }
+        };
+
+        JsonSerializerOptions options = new JsonSerializerOptions { };
+        string jsonString = JsonSerializer.Serialize(resultDictionary, options);
+
+        string filePath = "result.json";
+        File.WriteAllText(filePath, jsonString);
+
+        Console.WriteLine($"\nРезультат успішно збережено у файл: {Path.GetFullPath(filePath)}");
+    }
+}
---
+++ ЗАвдання 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+
+        List<int> numbers = new List<int> { 5, 12, -15, 45, 105, 99, 8, -50, 0 };
+
+        var targetNumbers = numbers.Where(x => x >= 10 && x <= 99);
+
+        int count = targetNumbers.Count();
+
+
+        double average = targetNumbers.DefaultIfEmpty(0).Average();
+
+        if (count == 0)
+        {
+            Console.WriteLine(0);
+            Console.WriteLine(0.0);
+        }
+        else
+        {
+            Console.WriteLine($"Кількість: {count}");
+            Console.WriteLine($"Середнє арифметичне: {average}");
+        }
+    }
+}
